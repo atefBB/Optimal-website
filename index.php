8da68248f6895b200b588cf4356972b641380c95
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2017-07-20 11:20:03 +0100

    change email address

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "ziedhajsalah@hotmail.com"; // this is your Email address
+    $to = "contact@optimal.com.tn"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $subject = $_POST['subject'];
