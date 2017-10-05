df30e565f8b682b8a0d8ad512489b661baab358f
Atef Ben Ali <atefBB@users.noreply.github.com>
2017-10-05 09:34:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 // Initialize the language code variable
 $lc = "";
-// Check to see that the global language server variable isset()
-// If it is set, we cut the first two characters from that string
+
+// Check if the global language server variable is set.
+// If it's the case, we cut the first two characters from that string
 if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
+{
     $lc = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+}
+
+// @todo Think about URL-rewrite.
 
 // Now we simply evaluate that variable to detect specific languages
-if ($lc == "fr") {
+if ($lc == "fr") 
+{
     header("location: /fr/index.php");
     exit();
 } else if ($lc == "ar") {
